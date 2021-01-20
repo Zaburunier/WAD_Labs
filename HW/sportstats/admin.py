eeed13e7f19d7905819b5da89b344cab633db15e
Zaburunier <lenyazaburunov@bk.ru>
2021-01-20 12:53:50 +0300

    ДЗ перенесено в отдельный репозиторий

--- HW/sportstats/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Tournament, Match, Team, User
-
-admin.site.register(Tournament)
-admin.site.register(Team)
-admin.site.register(Match)
-admin.site.register(User)
